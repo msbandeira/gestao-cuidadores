f137d501d180d1a8dc11f7fbc1589ef4fff0b89a
Marcello de Souza Bandeira <92650668+msbandeira@users.noreply.github.com>
2026-05-11 16:56:30 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+}
